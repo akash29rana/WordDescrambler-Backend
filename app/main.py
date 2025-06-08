eca590f491beb39d3f1893773fbf7d4f692bf70c
AKASH RANA <akashrana@AKASHs-Laptop.local>
2025-06-08 11:59:07 +0530

    handle cases

--- app/main.py
+++ app/main.py
@@ -23,10 +23,15 @@ async def unscramble(word: str,
     end_with: Optional[str] = Query(None, alias="end"),
     contains: Optional[str] = Query(None),
     length: Optional[int] = Query(None)):
+    word = word.lower()
+    start_with = start_with.lower() if start_with else None
+    end_with = end_with.lower() if end_with else None
+    contains = contains.lower() if contains else None
     return find_valid_words(word, start_with, end_with, contains, length)
 
 @app.get("/meaning")
 async def get_meaning(word: str):
+    word = word.lower()
     print(get_meaning_of_word(word))
     return get_meaning_of_word(word)
 
